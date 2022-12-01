80f5c77a97d76099259490197848293fbaa6d5b4
Kass Daniello <zertroz@gmail.com>
2022-11-30 21:34:23 -0500

    Add book.js and warmup

--- src/book.js
+++ src/book.js
@@ -1,12 +1,45 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+};
+
+function buildMainCharacter(characterName, characterAge, characterPronouns) {
+  var characterObject = {
+    name: characterName,
+    age: characterAge,
+    pronouns: characterPronouns
+  };
+  return characterObject;
+};
+
+function saveReview(review, array1) {
+  if (array1.includes(review) === false) {
+    array1.push(review);
+  };
+};
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+};
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    genre: genre,
+    pageCount: calculatePageCount(bookTitle)
+  };
+  return book
+};
+
+function editBook(bookTitle) {
+  bookTitle.pageCount = bookTitle.pageCount * .75
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }

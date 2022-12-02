2c14050ab2f4905fe81bb73463a551f124fe2504
Kass Daniello <zertroz@gmail.com>
2022-12-01 20:44:04 -0500

    Finish shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,44 @@
+function shelfBook(book, shelfName) {
+  if (shelfName.length <= 2) {
+    shelfName.unshift(book)
+  }
+}
+
+function unshelfBook(book, shelfName) {
+  for (i = 0; i < shelfName.length; i++) {
+    if (shelfName[i].title === book) {
+      return shelfName.splice(i, 1)
+    }
+  }
+}
+
+function listTitles(shelfName) {
+  var onShelf = []
+  for (i = 0; i < shelfName.length; i++) {
+    onShelf.push(shelfName[i].title)
+  }
+  return onShelf.join(", ")
+}
+
+function searchShelf(shelfName, name) {
+  var loop = true
+  for (i = 0; i < shelfName.length; i++) {
+    var book = ""
+    book += shelfName[i].title
+    if (book.includes(name)) {
+      return true
+    } else {
+      loop = false
+    }
+  }
+  if (loop === false) {
+    return loop;
+  };
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
